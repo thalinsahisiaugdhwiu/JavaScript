dac6341ae7edf94fcb7ea999e09a1bdd8c2107dc
Thalita Alves <thalitaalves783@outlook.com>
2022-11-23 09:42:58 -0300

    condicionais

---
+++ favelaware/semafaro.js
@@ -0,0 +1,9 @@
+var cor = "Vermelho"
+
+if (cor == "Verde"){
+    console.log('Prossiga')
+}else if(cor == "Amarelo"){
+    console.log('Atenção')
+}else if(cor == "Vermelho"){
+    console.log('Pare')
+}
