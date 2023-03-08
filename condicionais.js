d7ab56bfef46683408e7355152cb3e8eb0cfab20
thalinsahisiaugdhwiu <116230698+thalinsahisiaugdhwiu@users.noreply.github.com>
2023-03-08 16:15:01 -0300

    aula

---
+++ condicionais.js
@@ -0,0 +1,8 @@
+// Função para somar dois valores
+function calcular(n1,n2)//Argumentos
+{
+    console.log(n1+n2)
+}
+calcular(10,10)//Parametros
+
+// Função para calcular a divisão de dois numeros
